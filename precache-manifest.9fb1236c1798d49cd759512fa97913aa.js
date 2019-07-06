414e0b7f18b989dfdc86dc989837446917dd2cf7
delphi1004 <delphi1004@hanmail.net>
2019-07-06 14:52:11 +0300

    Updates

---
+++ precache-manifest.9fb1236c1798d49cd759512fa97913aa.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b214bc839aa918e0f9dd02dae61737b2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1a169e2e412c3cf162b3",
+    "url": "/portfolio/static/css/main.9caff0b6.chunk.css"
+  },
+  {
+    "revision": "8384258d00bc92917a20",
+    "url": "/portfolio/static/js/2.77993835.chunk.js"
+  },
+  {
+    "revision": "1a169e2e412c3cf162b3",
+    "url": "/portfolio/static/js/main.9ca3d00b.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "341ddcafb07ae262b8ff4c3042760fdc",
+    "url": "/portfolio/static/media/3dmodeling_01.341ddcaf.png"
+  },
+  {
+    "revision": "d66218efbcda2d7cd8e8e4d96c672d21",
+    "url": "/portfolio/static/media/3dmodeling_02.d66218ef.png"
+  },
+  {
+    "revision": "fe09eda688f11d7e796ca870189753e9",
+    "url": "/portfolio/static/media/3dmodeling_03.fe09eda6.png"
+  },
+  {
+    "revision": "c97232df97b22970dfba7a6d3a79752d",
+    "url": "/portfolio/static/media/3dmodeling_04.c97232df.png"
+  },
+  {
+    "revision": "260f216df4bd74173ee6176d0b30a69a",
+    "url": "/portfolio/static/media/3dmodeling_05.260f216d.png"
+  },
+  {
+    "revision": "a58db9345ff725e8c85138c492b58816",
+    "url": "/portfolio/static/media/3dmodeling_06.a58db934.png"
+  },
+  {
+    "revision": "ff343b8fa68bea3d9be0d5f488ca6d53",
+    "url": "/portfolio/static/media/3dmodeling_07.ff343b8f.png"
+  },
+  {
+    "revision": "446a8ae66eb56ed375330aaf873b1a7b",
+    "url": "/portfolio/static/media/3dmodeling_08.446a8ae6.png"
+  },
+  {
+    "revision": "548e6b9a34216650b659379dccb3e8db",
+    "url": "/portfolio/static/media/3dmodeling_09.548e6b9a.png"
+  },
+  {
+    "revision": "bd9f06a7cf60cb8e9a36204f8c76a9e8",
+    "url": "/portfolio/static/media/3dmodeling_10.bd9f06a7.png"
+  },
+  {
+    "revision": "64aab09bb4b15d7769a03bcffefc09e1",
+    "url": "/portfolio/static/media/3dmodeling_11.64aab09b.png"
+  },
+  {
+    "revision": "e8ecbc1ab8e58f7db31fc93d5b7d1428",
+    "url": "/portfolio/static/media/3dmodeling_12.e8ecbc1a.png"
+  },
+  {
+    "revision": "bbfe3e941de01ab44443ec5c63076b69",
+    "url": "/portfolio/static/media/JohnLee_CV.bbfe3e94.pdf"
+  },
+  {
+    "revision": "0dbc6876b6c30d35a139e382f83babc0",
+    "url": "/portfolio/static/media/Korea_Exhibition_01.0dbc6876.png"
+  },
+  {
+    "revision": "500b2228184934080f139d10cb4afb39",
+    "url": "/portfolio/static/media/Korea_Exhibition_02.500b2228.png"
+  },
+  {
+    "revision": "5d58b1ce4ee527b6489752dcb1075e59",
+    "url": "/portfolio/static/media/Korea_Exhibition_03.5d58b1ce.png"
+  },
+  {
+    "revision": "a6a5fe4ae0ff014ac8d9714e199e2506",
+    "url": "/portfolio/static/media/Korea_Exhibition_04.a6a5fe4a.JPG"
+  },
+  {
+    "revision": "82d6874715c883f2e6df5d5c15ffd234",
+    "url": "/portfolio/static/media/Korea_Exhibition_05.82d68747.JPG"
+  },
+  {
+    "revision": "c0568bfa128f8b6605b7ac886b4f15ba",
+    "url": "/portfolio/static/media/Korea_Exhibition_10.c0568bfa.JPG"
+  },
+  {
+    "revision": "a7631a37b65392cfc8e817b9c6202581",
+    "url": "/portfolio/static/media/SeeingSound_01.a7631a37.JPG"
+  },
+  {
+    "revision": "1554e66c5a459ed985d381f365800a01",
+    "url": "/portfolio/static/media/SeeingSound_02.1554e66c.JPG"
+  },
+  {
+    "revision": "a8fe524af34a7a335a68506873a129fe",
+    "url": "/portfolio/static/media/SeeingSound_03.a8fe524a.JPG"
+  },
+  {
+    "revision": "2c115dc0998bcf7e6071c3a01432748e",
+    "url": "/portfolio/static/media/SeeingSound_04.2c115dc0.JPG"
+  },
+  {
+    "revision": "c5bc84bf424e03b9b726bac46df5b60d",
+    "url": "/portfolio/static/media/SeeingSound_09.c5bc84bf.JPG"
+  },
+  {
+    "revision": "18b1930427800fe96091547bf4e12213",
+    "url": "/portfolio/static/media/SeeingSound_10.18b19304.JPG"
+  },
+  {
+    "revision": "341ddcafb07ae262b8ff4c3042760fdc",
+    "url": "/portfolio/static/media/SeeingSound_11.341ddcaf.png"
+  },
+  {
+    "revision": "b62de102cc969d9a22668bdecba6e181",
+    "url": "/portfolio/static/media/SeeingSound_12.b62de102.JPG"
+  },
+  {
+    "revision": "5fe057cd3a8bd26dc13651a34fc1bf62",
+    "url": "/portfolio/static/media/aqua_01.5fe057cd.png"
+  },
+  {
+    "revision": "68acb0f142e8bfb09be55124f4258f38",
+    "url": "/portfolio/static/media/aqua_02.68acb0f1.png"
+  },
+  {
+    "revision": "81e65aef8c82090078709ce173fb13e2",
+    "url": "/portfolio/static/media/aqua_03.81e65aef.JPG"
+  },
+  {
+    "revision": "66347ca66114312287ed4fc770c40ff1",
+    "url": "/portfolio/static/media/aqua_04.66347ca6.JPG"
+  },
+  {
+    "revision": "dc393fdbc5016463bb39a1c7d8b5e17b",
+    "url": "/portfolio/static/media/aqua_05.dc393fdb.JPG"
+  },
+  {
+    "revision": "2c28990f3b92553983227a5d5453f9cd",
+    "url": "/portfolio/static/media/aqua_06.2c28990f.JPG"
+  },
+  {
+    "revision": "5b2952bbae0dbc150b572c7618290abf",
+    "url": "/portfolio/static/media/aqua_07.5b2952bb.JPG"
+  },
+  {
+    "revision": "3b8d9fc1f840ff8ba3fdb475ff621879",
+    "url": "/portfolio/static/media/game_controller_01.3b8d9fc1.JPG"
+  },
+  {
+    "revision": "3ce80088f78b6131bc20c1d8dba2ddf1",
+    "url": "/portfolio/static/media/game_controller_02.3ce80088.JPG"
+  },
+  {
+    "revision": "790477f7674cc804397c4e52e750d130",
+    "url": "/portfolio/static/media/game_controller_03.790477f7.JPG"
+  },
+  {
+    "revision": "2f0ecd9ee4951a15085dd2aab0908492",
+    "url": "/portfolio/static/media/naturalsound_05.2f0ecd9e.PNG"
+  },
+  {
+    "revision": "b2edf75ce9ae6cb649fb37619520dcc1",
+    "url": "/portfolio/static/media/shape_01.b2edf75c.png"
+  },
+  {
+    "revision": "fde8ad6d9ac84c9ca187874f56aa53c5",
+    "url": "/portfolio/static/media/shape_02.fde8ad6d.png"
+  },
+  {
+    "revision": "bc989fb1105cbc10a5684d4bb0262799",
+    "url": "/portfolio/static/media/shape_03.bc989fb1.png"
+  },
+  {
+    "revision": "b2ea7419d45b06e09138c530b7a388ec",
+    "url": "/portfolio/static/media/shape_04.b2ea7419.png"
+  },
+  {
+    "revision": "a1cf465406a3b9a586372ab76d0deb23",
+    "url": "/portfolio/static/media/softwaredevelopment_01.a1cf4654.png"
+  },
+  {
+    "revision": "0f6aa3f4973e5af5eacba8dcf1d952e1",
+    "url": "/portfolio/static/media/softwaredevelopment_03.0f6aa3f4.png"
+  },
+  {
+    "revision": "dad7fce32509389ff882dfe2024e4ce5",
+    "url": "/portfolio/static/media/softwaredevelopment_04.dad7fce3.png"
+  },
+  {
+    "revision": "47b841593abba2e3d3a40b3a11c47357",
+    "url": "/portfolio/static/media/softwaredevelopment_05.47b84159.png"
+  },
+  {
+    "revision": "238d2de12ef7390787832c7a3722d44f",
+    "url": "/portfolio/static/media/softwaredevelopment_06.238d2de1.png"
+  },
+  {
+    "revision": "f97f05d4428de7f7e7c1ee57b389c95d",
+    "url": "/portfolio/static/media/softwaredevelopment_07.f97f05d4.png"
+  },
+  {
+    "revision": "228e76efcb77d8b3ba5782637717aaae",
+    "url": "/portfolio/static/media/soundvis_01.228e76ef.JPG"
+  },
+  {
+    "revision": "101f62c71ee980fc4b635f4acf15c9c4",
+    "url": "/portfolio/static/media/soundvis_02.101f62c7.JPG"
+  },
+  {
+    "revision": "3d49239d7cd1a29f1ad6e7f9704d1011",
+    "url": "/portfolio/static/media/soundvis_03.3d49239d.JPG"
+  }
+]);
